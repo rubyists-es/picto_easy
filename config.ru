4bde46a66d05a1747533d021eeb7cefaf7497637
Mariano Giagante <mgiagante@protonmail.com>
2018-05-21 20:43:03 -0300

    Adds Cuba app example code.

---
+++ config.ru
@@ -0,0 +1,4 @@
+# cat config.ru
+require "./picto_easy"
+
+run Cuba
---
+++ picto_easy.rb
@@ -0,0 +1,19 @@
+# cat hello_world.rb
+require "cuba"
+require "cuba/safe"
+
+Cuba.use Rack::Session::Cookie, :secret => "__a_very_long_string__"
+
+Cuba.plugin Cuba::Safe
+
+Cuba.define do
+  on get do
+    on "hello" do
+      res.write "Hello world!"
+    end
+
+    on root do
+      res.redirect "/hello"
+    end
+  end
+end
