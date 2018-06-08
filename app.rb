4e2e8bd2de03ac0dab1e86a32620d6b83e026f88
Mariano Giagante <mgiagante@protonmail.com>
2018-06-08 19:41:01 -0300

    Requires any controllers and models found in /controllers and /models directories.

--- app.rb
+++ app.rb
@@ -5,6 +5,11 @@ require "mote/render"
 # Load plugins for Cuba                                                                                                                                 
 Cuba.plugin Mote::Render 
 
+# Require all model and controller files, including sub-directories.
+%w[models controllers].each do |folder|
+  Dir["./#{folder}/**/*.rb"].each { |file| require_relative file }
+end
+
 # Here's where main routing definitions are: the main Cuba app.
 Cuba.define do
   on root do
